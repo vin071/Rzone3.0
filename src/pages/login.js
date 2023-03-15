75c7c06641935503b5735c0dc00804a18abf4fca
vin071 <e0969686@gmail.com>
2023-03-15 22:06:22 +0800

    modified login, now able to process wrong input

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,26 +18,71 @@ import { Component } from 'react';
 import { useState } from 'react';
 import { Box } from '@chakra-ui/react'
 import login_data from '../data/login_data';
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
 
-const Login = () => {
-  const [nusnetid, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [found, setFound] = useState(false);
-  const handleSubmit = () => {
-    console.log("Updated")
-    for (let i=0; i < login_data.length; i++) {
-      if (login_data[i][nusnetid] == this.nusnetid) {
-        if (login_data[i][password] == this.password) {
-          this.setFound(true)
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setId = this.setId.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+    this.state = {
+      nusnetId: "",
+      password: "",
+      fail: false,
+      found: false,
+      logindata: login_data
+    }
+  }
+  setId(val) {
+    this.setState({
+      nusnetId: val.target.value
+    })
+  }
+  setPassword(val) {
+    this.setState({
+      password: val.target.value
+    })
+  }
+  // const [nusnetid, setId] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [found, setFound] = useState(false);
+    // 2. Wrap ChakraProvider at the root of your app
+  handleSubmit() {
+    this.setState({
+      found: false
+    })
+    for (let i=0; i < this.state.logindata.length; i++) {
+      if (this.state.logindata[i]["nusnetid"] == this.state.nusnetid) {
+        if (this.state.logindata[i]["password"] == this.state.password) {
+          this.setState({
+            found: true,
+            fail: false
+          })
       }
     }
   };
+  if (!this.state.found) {
+    this.setState({
+      fail: true
+    })
+  } 
+  console.log("Updated")
 }
-  const placeholderSuccess = <p>Login is successful</p>;
-  const extra = <p>{nusnetid}</p>;
-  const placeholderFail = <p></p>
-    // 2. Wrap ChakraProvider at the root of your app
-  const report = "RZone is Raffles Hall's student activities portal, developed and maintained by ComMotion. For troubleshooting and bug report, please contact e0773508@u.nus.edu."
+  render() {
+    const placeholderSuccess = <p></p>;
+    const extra = <p>{this.state.nusnetId}</p>;
+    const loginFail = <Alert status='error'>
+    <AlertIcon />
+    <AlertTitle>Login fail</AlertTitle>
+    <AlertDescription>Wrong NUSNETID or NUSNET Password</AlertDescription>
+  </Alert>;
+    const report = "RZone is Raffles Hall's student activities portal, developed and maintained by ComMotion. For troubleshooting and bug report, please contact e0773508@u.nus.edu."
   return (
     <div>
       <Container width="100%">
@@ -46,15 +91,14 @@ const Login = () => {
       <Container maxWidth={820} width='100%' h='10' style={{display:"flex", flexDirection: "column", flexWrap: "wrap"}}>
         <Box width="45vw" maxWidth={400} minWidth={100} align="right" style={{padding:"10px"}}>
         <Text fontSize='2xl' style={{display: "flex", alignItems: "end", height: "60px", padding:"20px", color: "teal"}}>RZone 3.0</Text>
-          <FormControl id="login" paddingLeft="20px" onSubmit={handleSubmit}>
-            <Input type='text' id="nusnetid" value={nusnetid} placeholder="NUSNET ID" onChange={({target}) =>setId(target.value)}/>
-            <Input type='password' id="password" placeholder="NUSNET Password" value={password} onChange={({target}) =>setPassword(target.value)}/>
-            <Button colorScheme='teal' variant='solid' type="submit" padding="20px">
+          <FormControl id="login" paddingLeft="20px" isRequired>
+            <Input type='text' id="nusnetid" value={this.state.nusnetId} placeholder="NUSNET ID" onChange={(this.setId)}/>
+            <Input type='password' id="password" placeholder="NUSNET Password" value={this.state.password} onChange={(this.setPassword)}/>
+            <Button colorScheme='teal' variant='solid' type="submit" padding="20px" onClick={this.handleSubmit}>
               LOGIN
             </Button>
           </FormControl>
-          {found ? placeholderSuccess : placeholderFail}
-          {found ? extra : placeholderFail}
+          {this.state.fail ? loginFail : placeholderSuccess}
         </Box>
       <Box width='45vw'  maxWidth={400} minWidth={100} style={{padding:"10px"}}>
         <Heading as='h6' size='lg' style={{display: "flex", alignItems: "center", height: "60px", paddingLeft:"20px", color: "white", backgroundColor: "teal"}}>Announcement</Heading>
@@ -63,6 +107,7 @@ const Login = () => {
       </Container>
     </div>
   )
+  }
 }
 
 
